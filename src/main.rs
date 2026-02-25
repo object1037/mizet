cd8ddad69126b58efa0c5e3b7c4d4d9b970262f1
object1037 <contact@object1037.dev>
2026-02-25 14:52:11 +0900

    use ssd1306 display

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,27 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
+use embassy_rp::i2c;
 use embassy_rp::peripherals::PIO0;
-use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::pio;
 use embassy_rp::pio_programs::rotary_encoder::{Direction, PioEncoder, PioEncoderProgram};
 use embassy_rp::{
     Peri,
     gpio::{self, AnyPin},
 };
 use embassy_time::Timer;
+use embedded_graphics::{
+    mono_font::{MonoTextStyleBuilder, ascii::FONT_6X13},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
 use gpio::{Input, Level, Output, Pull};
+use ssd1306::{I2CDisplayInterface, Ssd1306, prelude::*};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-   PIO0_IRQ_0 => InterruptHandler<PIO0>;
+   PIO0_IRQ_0 => pio::InterruptHandler<PIO0>;
 });
 
 #[embassy_executor::task]
@@ -50,13 +58,51 @@ async fn handle_encoder(mut encoder: PioEncoder<'static, PIO0, 0>) {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    info!("Starting...");
     let p = embassy_rp::init(Default::default());
-    let Pio {
-        mut common, sm0, ..
-    } = Pio::new(p.PIO0, Irqs);
 
+    // PIO and encoder init
+    let pio::Pio {
+        mut common, sm0, ..
+    } = pio::Pio::new(p.PIO0, Irqs);
     let prg = PioEncoderProgram::new(&mut common);
     let encoder = PioEncoder::new(&mut common, sm0, p.PIN_13, p.PIN_14, &prg);
+    info!("Configured PIO");
+
+    // I2C init
+    let sda = p.PIN_16;
+    let scl = p.PIN_17;
+    let mut i2c_config = i2c::Config::default();
+    i2c_config.frequency = 400_000;
+    i2c_config.sda_pullup = false;
+    i2c_config.scl_pullup = false;
+    let i2c_bus = i2c::I2c::new_blocking(p.I2C0, scl, sda, i2c_config);
+    info!("Configured I2C");
+
+    // Display init
+    let display_interface = I2CDisplayInterface::new(i2c_bus);
+    let mut display = Ssd1306::new(
+        display_interface,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    )
+    .into_buffered_graphics_mode();
+    display.init().unwrap();
+    display.set_display_on(true).unwrap();
+    Timer::after_millis(100).await;
+    info!("Configured Display");
+
+    // Display text
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X13)
+        .text_color(BinaryColor::On)
+        .build();
+    let hello_text = Text::with_baseline("Hello Rust!", Point::zero(), text_style, Baseline::Top);
+    let pressed_text =
+        Text::with_baseline("Button Pressed", Point::zero(), text_style, Baseline::Top);
+
+    hello_text.draw(&mut display).unwrap();
+    display.flush().unwrap();
 
     spawner.spawn(blink(p.PIN_25.into())).unwrap();
     spawner.spawn(handle_encoder(encoder)).unwrap();
@@ -64,8 +110,15 @@ async fn main(spawner: Spawner) {
     let mut button = Input::new(p.PIN_23, Pull::Up);
     loop {
         button.wait_for_low().await;
+        display.clear_buffer();
+        pressed_text.draw(&mut display).unwrap();
+        display.flush().unwrap();
         info!("Button Pressed");
+
         button.wait_for_high().await;
+        display.clear_buffer();
+        hello_text.draw(&mut display).unwrap();
+        display.flush().unwrap();
         info!("Button Released");
     }
 }
